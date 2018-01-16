06c6c46c1a12554af7f7231b3a62a0509c43f9a9
unknown <kumargaurav9013@gmail.com>
2018-01-16 22:05:12 +0530

    Connect and Disconnect Modules

--- server/server.js
+++ server/server.js
@@ -1,17 +1,29 @@
 const path = require('path');
 const express = require('express');
-
-const app = express();
+const socketIO = require('socket.io');
+const http = require('http');
 
 const port = process.env.PORT || 3000;
-
 const publicPath = path.join(__dirname, '../public');
-
-console.log(publicPath);
+const app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
 
 
 app.use(express.static(publicPath));
 
-app.listen(port, () => {
+io.on('connection', (socket) => {
+    console.log('connected to a new user');
+
+    socket.on('disconnect', () => {
+        console.log('Dicsonnected from the user');
+    });
+});
+
+
+
+
+
+server.listen(port, () => {
     console.log(`App is starting on the port ${port}`);
 })
